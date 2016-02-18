737e0bedd0f04b02a441956a5724d61c7f4674dc
Kyler Brown <kylerjbrown@gmail.com>
2016-02-18 17:17:24 -0600

    started new file cluster.py

---
+++ cluster.py
@@ -0,0 +1,12 @@
+#/usr/bin/python
+
+# cluster
+# Kyler Brown (kjbrown@uchicago.edu)
+#
+# Takes a segmented output from segment.py and clusters the segments based on chosen features
+
+
+
+if __name__ == "__main__":
+    import argparse
+    pass
